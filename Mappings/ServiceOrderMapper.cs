81c3edffb3974c8402ea670c65fa1c18c1860879
Pedro Cabral <pedroscabral04@gmail.com>
2026-01-13 16:38:03 -0300

    fix: Update ServiceOrderMapper to include Customer mapping in ToDtoWithRelations method

--- Mappings/ServiceOrderMapper.cs
+++ Mappings/ServiceOrderMapper.cs
@@ -13,12 +13,14 @@ public partial class ServiceOrderMapper
     // ServiceOrder mappings
     public partial ServiceOrderDto ToDto(ServiceOrder order);
 
-    [MapProperty(nameof(ServiceOrder.Customer.Name), nameof(ServiceOrderDto.Customer))]
     [MapProperty(nameof(ServiceOrder.User.UserName), nameof(ServiceOrderDto.UserName))]
     public partial ServiceOrderDto ToDtoWithRelations(ServiceOrder order);
 
     public partial ServiceOrder ToEntity(CreateServiceOrderDto dto);
     public partial void UpdateEntity(UpdateServiceOrderDto dto, ServiceOrder entity);
+
+    // Helper mappings
+    private partial CustomerMiniDto? MapCustomer(Models.Sales.Customer? customer);
 
     // ServiceOrderItem mappings
     [UserMapping(Default = true)]
